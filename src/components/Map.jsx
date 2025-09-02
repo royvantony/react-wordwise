73bf85e18ac1b7fb8fdafd8f795c17d7e8a21ed1
ROY ANTONY <royvantony@gmail.com>
2025-09-02 12:14:24 +1000

    Initial

---
+++ src/components/Map.jsx
@@ -0,0 +1,26 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
+function Map() {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form");
+      }}
+    >
+      <span>
+        Position: {lat},{lng}
+      </span>
+      <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
+        change pos
+      </button>
+    </div>
+  );
+}
+
+export default Map;
